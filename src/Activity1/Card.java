812d05de88166d35defe5ebf61e07eb1b612699a
BT_1N3_06 <BT_1N3_06>
2019-01-09 11:43:15 -0500

    vjhvjh

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -34,7 +34,9 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
     }
 
 
@@ -43,8 +45,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+		return suit;
     }
 
     /**
@@ -52,8 +53,7 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return rank;
     }
 
     /**
@@ -61,8 +61,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return pointValue;
     }
 
     /** Compare this card with the argument.
@@ -72,8 +71,10 @@ public class Card {
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        if (this.rank == otherCard.rank && this.suit == otherCard.suit && this.pointValue == otherCard.pointValue) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -88,8 +89,8 @@ public class Card {
      */
     @Override
     public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        String pointValueString = Integer.toString(pointValue);
+        return rank + " of " + suit + "(point value = " + pointValueString + ")";
     }
 }
 
--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,7 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    Card card1 = ;
+    Card card2 = ;
+    Card card3 = ;
 }
